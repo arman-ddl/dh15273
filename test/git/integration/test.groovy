7017ec0b1f4844f3280fd30390c7e201a776cc37
arman-ddl <122062464+arman-ddl@users.noreply.github.com>
2023-10-24 14:24:56 -0700

    Add controller test script

---
+++ test/git/integration/test.groovy
@@ -0,0 +1,13 @@
+package test.git.integration
+
+println "Git integration"
+
+class GitIntegrationClass {
+  GitIntegration() {
+    println "Git integration class constructor"
+  }
+}
+
+void gitIntegrationMethod() {
+  println "Git integration method"
+}
